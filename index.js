36b8561f148ec43f863852fb9251a89a62406431
Christopher Wattaul <chris.wattaul@gmail.com>
2020-05-06 17:23:48 +0200

    Add meme scraper

---
+++ index.js
@@ -0,0 +1,28 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const imageDownloader = require('node-image-downloader');
+
+request('https://memegen.link/examples', function (err, res, html) {
+  const $ = cheerio.load(html);
+  $('.row a').each((i, el) => {
+    const url = 'https://memegen.link';
+    const item = $(el).text();
+    const img = $(el).attr('href').replace(/[']/g, '').split('?')[0];
+
+    const link = url + img;
+
+    if (i < 10) {
+      const firstTen = link;
+      imageDownloader({
+        imgs: [
+          {
+            uri: link,
+            filename: '0' + i,
+          },
+        ],
+        dest: './memes',
+      });
+      console.log(firstTen);
+    }
+  });
+});
